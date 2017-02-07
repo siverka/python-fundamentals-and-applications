c341e64ed0eec06b1be5cc75d6dcb02264e3b21d
Maria Korobeynikova <siverka@gmail.com>
2017-02-07 18:15:14 +0300

    resolved step 1.3.15

--- Module 1/Step 1.3.15.py
+++ Module 1/Step 1.3.15.py
@@ -1,8 +1,10 @@
-def c_n_k(n: int, k: int):
+def C(n: int, k: int):
     if k == 0:
         return 1
+    elif k > n:
+        return 0
     else:
-        return(c_n_k(n-1, k) + c_n_k(n-1, k-1))
+        return(C(n-1, k) + C(n-1, k-1))
 
-print(c_n_k(3, 2))
-print(c_n_k(10, 5))
+n, k = map(int, input().split())
+print(C(n, k))
