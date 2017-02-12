f5966b6aa34e54c61ca293ae10b894aa1d8cd059
Maria Korobeynikova <siverka@gmail.com>
2017-02-12 15:05:26 +0300

    resolved step 1.6.7

--- Module 1/Step 1.6.7.py
+++ Module 1/Step 1.6.7.py
@@ -5,65 +5,58 @@ def add_class(lst: list):
     if len(lst) == 1:
         classes[lst[0]] = []
     else:
-        indx = lst.index(':')
-        classes[lst[0]] = lst[indx + 1:]
+        classes[lst[0]] = lst[lst.index(':') + 1:]
 
 
-def get_children(parent):
-    res = []
-    for key, value in classes.items():
-        if parent in value:
-            res.append(key)
-    return res
+def is_parent(parent: str, children: list):
+    # классы вообще есть,
+    if (parent not in classes.keys()) or (children[0] not in classes.keys()):
+        return "No"
 
-
-def is_parent(parent, child):
     # один и тот же класс
-    print('Parent %s, child %s' % (parent, child))
-    if (parent == child) and (child in classes.keys()):
-        print('Equals')
+    if parent in children:
         return "Yes"
+
     # прямой предок
-    if parent in classes[child]:
-        print('Straight')
+    union_parents = []
+    for child in children:
+        union_parents.extend(classes[child])
+    union_parents = list(set(union_parents))
+    if parent in union_parents:
         return "Yes"
+
     # уходим вглубь
-    for subparent in get_children(parent):
-        print('Subparent %s, child %s' % (subparent, child))
-        if is_parent(subparent, child):
-            return "Yes"
-    # все остальное
-    return "No"
+    if len(union_parents) == 0:
+        return "No"
+    return is_parent(parent, union_parents)
 
 
-'''n = int(input())
+n = int(input())
 for i in range(n):
     add_class(input().split())
-'''
-# classes = {'A': [], 'B': ['A'], 'C': ['A'], 'D': ['B', 'C']}
-classes = dict(G=['F'], A=[], B=['A'], C=['A'], D=['B', 'C'], E=['D'], F=['D'], X=[], Y=['X', 'A'], Z=['X'],
-               V=['Z', 'Y'], W=['V'])
 
-print(classes)
-
-'''q = int(input())
+q = int(input())
 for i in range(q):
-    parent, child = input().split()
-    print(is_parent(parent, child))
-'''
+    p, c = input().split()
+    print(is_parent(p, [c]))
 
+# test data
+'''classes = dict(G=['F'], A=[], B=['A'], C=['A'], D=['B', 'C'], E=['D'], F=['D'], X=[], Y=['X', 'A'], Z=['X'],
+               V=['Z', 'Y'], W=['V'])
 lst = [
     'A G',  # Yes   # A предок G через B/C, D, F
-    # 'A Z',      # No    # Y потомок A, но не Y
-    # 'A W',      # Yes   # A предок W через Y, V
-    # 'X W',      # Yes   # X предок W через Y, V
-    # 'A X',      # No    # классы есть, но они нет родства :)
-    #    'X X',      # Yes   # родитель он же потомок
-    #    '1 1',      # No    # несуществующий класс
-    #    'X QWE'    # No    # нет такого класса QWE
+    'A Z',      # No    # Y потомок A, но не Y
+    'A W',      # Yes   # A предок W через Y, V
+    'X W',      # Yes   # X предок W через Y, V
+    'A X',      # No    # классы есть, но они нет родства :)
+    'X X',      # Yes   # родитель он же потомок
+    '1 1',      # No    # несуществующий класс
+    'X QWE'    # No    # нет такого класса QWE
 ]
 
 for e in lst:
     p, c = e.split()
-    print(p, c)
-    print(is_parent(p, c))
+    print(p, c, end= ' ')
+    print(is_parent(p, [c]))
+
+'''
