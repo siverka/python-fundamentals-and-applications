19e7da5287b68d6cd84653ade75d6a6239103544
Maria Korobeynikova <siverka@gmail.com>
2017-02-13 22:40:07 +0300

    resolved step 2.1.7

---
+++ Module 2/Step 2.1.7.py
@@ -0,0 +1,66 @@
+classes = {}
+
+
+def add_class(lst: list):
+    if len(lst) == 1:
+        classes[lst[0]] = []
+    else:
+        classes[lst[0]] = lst[lst.index(':') + 1:]
+
+
+def is_parent(parent: str, children: list):
+    # классы вообще есть,
+    if (parent not in classes.keys()) or (children[0] not in classes.keys()):
+        return "No"
+
+    # один и тот же класс
+    if parent in children:
+        return "Yes"
+
+    # прямой предок
+    union_parents = []
+    for child in children:
+        union_parents.extend(classes[child])
+    union_parents = list(set(union_parents))
+    if parent in union_parents:
+        return "Yes"
+
+    # уходим вглубь
+    if len(union_parents) == 0:
+        return "No"
+    return is_parent(parent, union_parents)
+
+
+n = int(input())
+for _ in range(n):
+    add_class(input().split())
+
+'''classes = {'ArithmeticError': [], 'ZeroDivisionError': ['ArithmeticError'], 'OSError': [],
+           'FileNotFoundError': ['OSError'], 'ConnectionError': ['OSError'], 'BrokenPipeError': ['ConnectionError']}
+print(classes)'''
+
+m = int(input())
+errors = []
+for _ in range(m):
+    errors.append(input())
+
+
+'''errors = ['ZeroDivisionError', 'OSError', 'ArithmeticError', 'FileNotFoundError',
+          'ConnectionError', 'BrokenPipeError']
+print(errors)'''
+
+m = len(errors)
+res = [False for _ in range(m)]
+
+
+for i in range(m):
+    for j in range(i+1, m):
+        #print(i, j, errors[i], errors[j], is_parent(errors[i], [errors[j]]))
+        if is_parent(errors[i], [errors[j]]) == "Yes":
+            res[j] = True
+#            print(errors[j])
+#print(*res)
+
+for i in range(m):
+    if res[i]:
+        print(errors[i])
